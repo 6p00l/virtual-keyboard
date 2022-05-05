c87211399c2ab9a60dec249c7a7e6102f7b5efb9
6p00l <nolf.91@gmail.com>
2022-05-05 22:55:32 +0300

    feat: add keyboard layout

---
+++ script.js
@@ -0,0 +1,162 @@
+let input = document.getElementById("input_main").value;
+function change(test) {
+  input = test;
+  console.log(input);
+}
+
+const keysEN = [
+  { "`": "Backquote" },
+  { 1: "Digit1" },
+  { 2: "Digit2" },
+  { 3: "Digit3" },
+  { 4: "Digit4" },
+  { 5: "Digit5" },
+  { 6: "Digit6" },
+  { 7: "Digit7" },
+  { 8: "Digit8" },
+  { 9: "Digit9" },
+  { 0: "Digit0" },
+  { "-": "Minus" },
+  { "=": "Equal" },
+  { Backspace: "Backspace" },
+  { Tab: "Tab" },
+  { q: "KeyQ" },
+  { w: "KeyW" },
+  { e: "KeyE" },
+  { r: "KeyR" },
+  { t: "KeyT" },
+  { y: "KeyY" },
+  { u: "KeyU" },
+  { i: "KeyI" },
+  { o: "KeyO" },
+  { p: "KeyP" },
+  { "[": "BracketLeft" },
+  { "]": "BracketRight" },
+  { "\\": "Backslash" },
+  { Delete: "Delete" },
+  { CapsLock: "CapsLock" },
+  { A: "KeyA" },
+  { S: "KeyS" },
+  { D: "KeyD" },
+  { F: "KeyF" },
+  { G: "KeyG" },
+  { H: "KeyH" },
+  { J: "KeyJ" },
+  { K: "KeyK" },
+  { L: "KeyL" },
+  { ";": "Semicolon" },
+  { "'": "Quote" },
+  { Enter: "Enter" },
+  { Shift: "ShiftLeft" },
+  { Z: "KeyZ" },
+  { X: "KeyX" },
+  { C: "KeyC" },
+  { V: "KeyV" },
+  { B: "KeyB" },
+  { N: "KeyN" },
+  { M: "KeyM" },
+  { ",": "Comma" },
+  { ".": "Period" },
+  { "/": "Slash" },
+  { Shift: "ShiftRight" },
+  { Control: "ControlLeft" },
+  { Meta: "MetaLeft" },
+  { Alt: "AltLeft" },
+  { " ": "Space" },
+  { Alt: "AltRight" },
+  { Meta: "MetaRight" },
+  { Control: "ControlRight" },
+  { ArrowLeft: "ArrowLeft" },
+  { ArrowUp: "ArrowUp" },
+  { ArrowRight: "ArrowRight" },
+  { ArrowDown: "ArrowDown" },
+];
+
+const keysRU = [
+  { ё: "Backquote" },
+  { 1: "Digit1" },
+  { 2: "Digit2" },
+  { 3: "Digit3" },
+  { 4: "Digit4" },
+  { 5: "Digit5" },
+  { 6: "Digit6" },
+  { 7: "Digit7" },
+  { 8: "Digit8" },
+  { 9: "Digit9" },
+  { 0: "Digit0" },
+  { "-": "Minus" },
+  { "=": "Equal" },
+  { Backspace: "Backspace" },
+  { Tab: "Tab" },
+  { й: "KeyQ" },
+  { ц: "KeyW" },
+  { у: "KeyE" },
+  { к: "KeyR" },
+  { е: "KeyT" },
+  { н: "KeyY" },
+  { г: "KeyU" },
+  { ш: "KeyI" },
+  { щ: "KeyO" },
+  { з: "KeyP" },
+  { х: "BracketLeft" },
+  { ъ: "BracketRight" },
+  { "\\": "Backslash" },
+  { Delete: "Delete" },
+  { CapsLock: "CapsLock" },
+  { Ф: "KeyA" },
+  { Ы: "KeyS" },
+  { В: "KeyD" },
+  { А: "KeyF" },
+  { П: "KeyG" },
+  { Р: "KeyH" },
+  { О: "KeyJ" },
+  { Л: "KeyK" },
+  { Д: "KeyL" },
+  { Ж: "Semicolon" },
+  { Э: "Quote" },
+  { Enter: "Enter" },
+  { Shift: "ShiftLeft" },
+  { Я: "KeyZ" },
+  { Ч: "KeyX" },
+  { С: "KeyC" },
+  { М: "KeyV" },
+  { И: "KeyB" },
+  { Т: "KeyN" },
+  { Ь: "KeyM" },
+  { Б: "Comma" },
+  { Ю: "Period" },
+  { ".": "Slash" },
+  { Shift: "ShiftRight" },
+  { Control: "ControlLeft" },
+  { Meta: "MetaLeft" },
+  { Alt: "AltLeft" },
+  { " ": "Space" },
+  { Control: "ControlLeft" },
+  { AltGraph: "AltRight" },
+  { Meta: "MetaRight" },
+  { Control: "ControlRight" },
+  { ArrowLeft: "ArrowLeft" },
+  { ArrowUp: "ArrowUp" },
+  { ArrowRight: "ArrowRight" },
+  { ArrowDown: "ArrowDown" },
+];
+
+const wrapper = document.createElement("div");
+wrapper.className = "wrapper";
+document.body.appendChild(wrapper);
+
+for (let i = 0; i < keysEN.length; i++) {
+  const container = document.createElement("div");
+  container.className = "button";
+
+  let key;
+  let code;
+  for (const k in keysEN[i]) {
+    key = k;
+    code = keysEN[i][k];
+  }
+  const key_div = `<div class='key ${code}' >${key}</div>`;
+  container.innerHTML = key_div;
+
+  wrapper.appendChild(container);
+}
